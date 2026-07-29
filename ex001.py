ad43a7738d36c4963ce57261bcdbd7f23c4fcd8f
Isaac Amorim <163526904+isaacamorimm@users.noreply.github.com>
2026-07-29 11:55:49 -0300

    feat: mais criação de classes

---
+++ ex001.py
@@ -0,0 +1,22 @@
+ # Declaração de Classe
+
+class Gafanhoto:
+    def __init__(self): # Método Construtor
+        # Atributos de Instância
+        self.nome = ""
+        self.idade = 0
+
+    # Métodos de Instância
+    def aniversario(self):
+        self.idade += 1
+
+    def mensagem(self):
+        return f"{self.nome} é um gafanhoto de {self.idade} anos de idade."
+
+# Declaração de Objeto
+g1 = Gafanhoto()
+g1.nome = "João"
+g1.idade = 5
+g1.aniversario()
+print(g1.mensagem())
+
