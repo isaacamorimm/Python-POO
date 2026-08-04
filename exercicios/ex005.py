24bc42e401c466b7aa3498201b2f96867f3d6b0a
Isaac Amorim <163526904+isaacamorimm@users.noreply.github.com>
2026-08-04 13:55:07 -0300

    add: Introdução às Heranças

---
+++ exercicios/ex005.py
@@ -0,0 +1,17 @@
+from rich import print, inspect
+from classesex005 import Aluno, Professor, Funcionario
+
+
+a1 = Aluno("José", 35, "Informática", "TODAS")
+a1.fazer_aniversario()
+a1.fazer_matricula()
+#inspect(a1, methods=True)
+
+p1 = Professor("Marcos", 17, "Biologia", "T01")
+p1.dar_aula()
+#inspect(a1, methods=True)
+
+f1 = Funcionario("Claudia", 27, "Secretária", "Secretaria")
+f1.fazer_aniversario()
+f1.bater_ponto()
+#inspect(f1, methods=True)
