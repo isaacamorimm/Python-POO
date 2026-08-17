b24661f2ef314389b99f492aa174a0c424bac492
Isaac Amorim <163526904+isaacamorimm@users.noreply.github.com>
2026-08-17 13:22:06 -0300

    feat: implement Diario class usage in main entry point

--- desafios/desafio029/__main__.py
+++ desafios/desafio029/__main__.py
@@ -3,12 +3,14 @@ from rich import print, inspect
 
 def main():
     d = Diario("Senha")
+    
+    d.senha = "123"
 
     d.escrever("Olá mundo!")
     d.escrever("oi")
     d.escrever("vai tomando")
 
-    d.ler("Senha")
+    d.ler("123")
 
     inspect(d, private=True, methods=True)
 
